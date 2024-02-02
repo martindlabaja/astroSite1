14ac9561b8aa4de6a18e3ad94b05fd2ec2e9e0b3
yoneda-lemma <99047868+yoneda-lemma@users.noreply.github.com>
2024-02-02 17:18:18 +0100

    more mobile styles

--- src/pages/posts/images/_ResizeToPublic.bat
+++ src/pages/posts/images/_ResizeToPublic.bat
@@ -1,5 +1,5 @@
 @echo off
 title Lets Resize it in Batch!
-magick.exe mogrify -resize 500x1000 -path ../../../../public/images *.jpg *.png
+magick.exe mogrify -resize 600x1000 -path ../../../../public/images *.jpg *.png
 echo Resized images saved into \public\images!
 pause
