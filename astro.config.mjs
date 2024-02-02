7533845e7ee89ece3d5e7a0f249dc98e64ba3eb6
yoneda-lemma <99047868+yoneda-lemma@users.noreply.github.com>
2024-02-02 17:11:58 +0100

    comming test both vercel and netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig({    
     site: "https://peppy-marshmallow-841b1d.netlify.app"
   });
+
+
+
+
+
+  
+
